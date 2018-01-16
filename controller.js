f6ae5f41a442606938d58a4fd308432d7e05dc10
Juyoung Moon <>
2018-01-16 23:51:38 +0900

    - todo add - todo load

--- controller.js
+++ controller.js
@@ -1,13 +1,29 @@
 window.onload = function () {
-  var list = [];
+  var listStorage = [];
   var todo = document.getElementById('todo');
 
   todo.addEventListener('keydown', function (e) {
     if(e.keyCode === 13) {
-      list.push(todo.value);
+      listStorage.push(todo.value);
       todo.value = '';
+
+      loadData(listStorage);
     }
   });
 
+  var loadData = function (list) {
+    var listStorage = list;
+    var listDom = document.getElementById('list');
 
+    if(!listStorage) {return;}
+
+    listStorage.forEach(function (item, index) {
+      var listItem = document.createElement('p');
+      var textItem = document.createTextNode(index+1 + '. ' + item);
+
+      listItem.className = 'list-item';
+      listItem.appendChild(textItem);
+      listDom.appendChild(listItem);
+    });
+  }
 };
