a092bc1c24eb802a334839bd89c18256488df47e
juyoungmoon <moon7313@douzone.com>
2018-01-18 08:46:37 +0900

    fix: checkbox uncheck status

--- public/controller.js
+++ public/controller.js
@@ -2,57 +2,60 @@ window.onload = function () {
   var listStorage = ['reading', 'writing'];
   var todo = document.getElementById('todo');
   var listDom = document.getElementById('list');
+
   function createListItem(index, name) {
-      var checkbox;
-      var removeButton;
-      var listItem;
-      var textItem;
-      var textArea;
-
-      checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-
-      checkbox.onclick = function () {
-        var self = this;
-        var checked = false;
-
-        console.log('self',self);
-
-        if(self.getAttribute('checked') !== true) {
-          checked = true;
-          console.log('inner set true', self.getAttribute('checked'));
-        }
-        console.log('outer set click', checked, typeof checkbox.getAttribute('checked'));
-        self.setAttribute('checked', checked);
-
-      };
-
-      removeButton = document.createElement('button');
-      removeButton.innerHTML = 'X';
-      removeButton.className = 'close-button';
-
-      removeButton.onclick = function () {
-        listStorage.forEach(function (item, index) {
-          if(item === name) {
-            listStorage.splice(index, 1);
-          }
-        });
-
-        listDom.removeChild(listItem);
-      };
-
-      listItem = document.createElement('div');
-      textArea = document.createElement('p');
-      textItem = document.createTextNode(name);
+      var listItem = document.createElement('div');
+      var textItem = document.createTextNode(name);
+      var textArea = document.createElement('p');
+      var checkbox = createCheckbox(listItem);
+      var removeButton = createRemoveButton(listItem);
 
       textArea.appendChild(textItem);
-
       listItem.className = 'list-item';
       listItem.appendChild(checkbox);
       listItem.appendChild(textArea);
       listItem.appendChild(removeButton);
 
       return listItem;
+  }
+
+  function createCheckbox() {
+      var checkbox;
+
+      checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+
+      checkbox.onclick = function () {
+          var self = this;
+          var checked = false;
+
+          if(self.getAttribute('checked') !== "true") {
+              checked = true;
+          }
+          self.setAttribute('checked', checked);
+      };
+
+      return checkbox;
+  }
+
+  function createRemoveButton(listItem) {
+      var removeButton;
+
+      removeButton = document.createElement('button');
+      removeButton.innerHTML = 'X';
+      removeButton.className = 'close-button';
+
+      removeButton.onclick = function () {
+          listStorage.forEach(function (item, index) {
+              if(item === name) {
+                  listStorage.splice(index, 1);
+              }
+          });
+
+          listDom.removeChild(listItem);
+      };
+
+      return removeButton
   }
 
   function loadData(list) {
